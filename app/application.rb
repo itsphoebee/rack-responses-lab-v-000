4a2e96f5557813117ec80e17d9682d5dddb752c8
Phoebe Lau <phoebelau7@gmail.com>
2018-01-05 18:18:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     if Time.now.hour < 12
-      resp.write "Good Morning! The time is now #{DateTime.now.strftime("%H:%M")}."
+      resp.write "Good Morning! The time is now #{DateTime.now.strftime("%I:%M")}."
     else
-      resp.write "Good Afternoon! The time is now #{DateTime.now.strftime("%H:%M")}."
+      resp.write "Good Afternoon! The time is now #{DateTime.now.strftime("%I:%M")}."
     end
     resp.finish
   end
